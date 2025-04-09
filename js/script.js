c872ae17ca56db45fa6c9ddd65e66b723d67c298
DavideGiuseppeCiliento <cilientodg@gmail.com>
2025-04-09 17:14:28 +0200

    Sistemato esercizio con il pulsante.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,21 @@
 //   lamp.src = "assets/img/yellow_lamp.png";
 // });
 
+// const lamp = document.getElementById("lampadina");
+// console.log(lamp);
+// lamp.addEventListener("click", () => {
+//   lamp.src.includes("white_lamp.png") ? (lamp.src = "assets/img/yellow_lamp.png") : (lamp.src = "assets/img/white_lamp.png");
+// });
+
 const lamp = document.getElementById("lampadina");
-console.log(lamp);
-lamp.addEventListener("click", () => {
-  lamp.src.includes("white_lamp.png") ? (lamp.src = "assets/img/yellow_lamp.png") : (lamp.src = "assets/img/white_lamp.png");
+const buttonEl = document.getElementById("bottone");
+console.log(buttonEl);
+buttonEl.addEventListener("click", () => {
+  if (lamp.src.includes("white_lamp.png")) {
+    lamp.src = "assets/img/yellow_lamp.png";
+    buttonEl.innerHTML = "Spegni";
+  } else {
+    lamp.src = "assets/img/white_lamp.png";
+    buttonEl.innerHTML = "Accendi";
+  }
 });
